836739ac9eb0185816cc895c51bee6081c9d2782
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-08-09 15:30:40 +0900

    テーブルにカラムの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
-    
+    @user = User.find(1)
   end
+
+  
+
 end
---
+++ db/migrate/20190809060511_add_column_to_product.rb
@@ -0,0 +1,5 @@
+class AddColumnToProduct < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :client_id, :string
+  end
+end
