20a27f80ca255a0263f7143b1fc285c899453928
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-08-14 19:07:56 +0900

    deviseのview作成、haml変換

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+  
   def index
   end
 
@@ -13,5 +16,9 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:drawing_number,:image,:memo,:finished_product,:client_name)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
